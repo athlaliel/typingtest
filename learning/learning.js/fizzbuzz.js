b61b54c5bf2a5af3c77d8a3c4cd9d78579eca7bd
athlaliel <athla8452@gmail.com>
2020-09-20 20:26:45 +0900

    fizz_buzz

--- learning/learning.js/fizzbuzz.js
+++ learning/learning.js/fizzbuzz.js
@@ -4,7 +4,18 @@ for (var i = 1; i <= 100; i++) {
   else if (i % 3 === 0)
   console.log("Fizz");
   else if (i % 5 === 0)
-  console.log("Buzz")
+  console.log("Buzz");
   else // iが3の倍数でも5の倍数でもない
+  console.log(i);
+}
+
+for (var i = 1; i <= 100; i++) {
+  if(i % 3 === 0 && i % 5 === 0)
+  console.log("Fizz,Buzz");
+  else if (i % 3 === 0)
+  console.log("Fizz");
+  else if (i % 5 === 0)
+  console.log("Buzz");
+  else
   console.log(i);
 }
