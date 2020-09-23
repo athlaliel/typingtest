102a9cec1576d2ba22adeb100a14c6af808baf0f
athlaliel <athla8452@gmail.com>
2020-09-23 13:28:51 +0900

    js_learning

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+import youtuber from "./foo";
+
+// youtuber.list.business.youtuber[0].teachProgramming();
+
+youtuber.plan();
+
+// youtuber.list.entertainment.makeSmile();
---
+++ javascript/jsbin1.js
@@ -0,0 +1,14 @@
+const foo = document.getElementById("foo")
+
+const fooWidth = foo.offsetWidth;
+
+const fooHeight = foo.offsetHeight;
+
+const fooArea = fooWidth * fooHeight;
+
+// # 変数名
+const foo = 100;
+
+let foo = 200;
+
+var foo = 300;
