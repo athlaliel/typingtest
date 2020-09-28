edb1ad4e9ac94aedf494b6fdbf915b0e025fa4e4
athlaliel <athla8452@gmail.com>
2020-09-29 02:04:43 +0900

    import-pattern

---
+++ javascript/import_export/index.js
@@ -0,0 +1,4 @@
+// import { name, name2 } from "./user.js";
+
+import "./side-effect.js";
+// document.body.textContent = name + name2;
---
+++ javascript/import_export/user.js
@@ -0,0 +1,4 @@
+// export const name = "ピカチュウ";
+// export const name2 = "ライチュウ";
+
+// export { name, name2 };
